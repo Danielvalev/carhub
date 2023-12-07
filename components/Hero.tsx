d719769a75542df4d53cdb6f302219ca658d2a80
Daniel Valev <danielvalev89@gmail.com>
2023-12-07 10:45:55 +0200

    carhub: added navbar and footer, added constants

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,6 +22,12 @@ const Hero = () => {
                     handleClick={hadnleScroll}    
                 />
             </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
+                </div>
+                <div className="hero__image-overlay" />
+            </div>
         </div>
     )
 }
